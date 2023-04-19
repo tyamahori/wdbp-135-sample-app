ccbfde87155843e28854db5bb7d6dd3a02054bd9
tyamahori <tyamahori@gmail.com>
2023-04-19 22:40:21 +0900

    :art: php artisan make:model SimpleMessage --all 追記

--- database/migrations/2023_04_19_133551_create_simple_messages_table.php
+++ database/migrations/2023_04_19_133551_create_simple_messages_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('simple_messages', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
             $table->timestamps();
         });
     }
